a8d300a70dbc4bca4a848e8cc3ffcea2ee1ba6fc
Caio Lucas <caiolucasprogramador@gmail.com>
2026-06-07 16:36:18 -0300

    fix: remove .env do repositório

---
+++ src/main/java/br/ufc/artgallery/conexao/Conexao.java
@@ -0,0 +1,20 @@
+package br.ufc.artgallery.conexao;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public class Conexao {
+
+        private static final Dotenv dotenv = Dotenv.load();
+        private static final String URL = dotenv.get("DB_URL");
+        private static final String USER = dotenv.get("DB_USER");
+        private static final String PASS = dotenv.get("DB_PASS");
+
+        public static Connection getConexao() throws SQLException {
+            return DriverManager.getConnection(URL, USER, PASS);
+        }
+    }
+}
