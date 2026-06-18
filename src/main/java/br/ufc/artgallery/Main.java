755f8aabef378403fb589d1377dcd3eb5d4a1f77
Caio Lucas <caiolucasprogramador@gmail.com>
2026-06-18 08:39:59 -0300

    final: inicializa MainFrame na classe principal e encerra fluxo da interface

--- src/main/java/br/ufc/artgallery/Main.java
+++ src/main/java/br/ufc/artgallery/Main.java
@@ -1,20 +1,26 @@
 package br.ufc.artgallery;
 
-import br.ufc.artgallery.conexao.Conexao;
-import br.ufc.artgallery.dao.ObraDAO;
-import br.ufc.artgallery.excecoes.NotaInvalidaException;
-import br.ufc.artgallery.excecoes.ObraNaoEncontradaException;
-import br.ufc.artgallery.models.Obra;
-import br.ufc.artgallery.models.PinturaDigital;
-
-import java.sql.Connection;
-import java.sql.SQLException;
+import br.ufc.artgallery.repository.RepositorioObraDB;
+import br.ufc.artgallery.ui.MainFrame;
+import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ObraNaoEncontradaException, NotaInvalidaException {
-        ObraDAO dao = new ObraDAO();
-        Obra obra = new PinturaDigital("Meu amorzão lindo: Bianca", "Deus", "FULL 16K HD ULTRA MAX", "Barro e minha costela");
-        PinturaDigital pd = (PinturaDigital) dao.buscarObra(1);
-        System.out.println(pd.exibirDetalhes());
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            try {
+                RepositorioObraDB repo = new RepositorioObraDB();
+                ArtGallery gallery = new ArtGallery(repo);
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+                MainFrame frame = new MainFrame(gallery);
+                frame.setVisible(true);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
+            }
+        });
+
     }
+
 }
